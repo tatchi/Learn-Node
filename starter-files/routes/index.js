e278f0a94f09fe31b395749eed37c05ec75dbd35
Corentin Leruth <clt@pulsar.be>
2017-06-15 22:23:04 +0200

    initial commit

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const result = { name: 'tatchi', cool: true };
+  // res.send('Hey! It works!');
+  res.json(result);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
