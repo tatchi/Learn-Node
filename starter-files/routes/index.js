2e4cbbe5e5a98bd7d53654b80651b182e3af8aa2
Corentin Leruth <clt@pulsar.be>
2017-07-02 12:15:20 +0200

    Ajax REST API 2

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -44,8 +44,13 @@ router.post('/account/forgot', catchErrors(authController.forgot))
 router.get('/account/reset/:token', catchErrors(authController.reset))
 router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update))
 
+router.get('/map', storeController.mapPage);
+
 //API
 
 router.get('/api/search', catchErrors(storeController.searchStores))
+router.get('/api/stores/near', catchErrors(storeController.mapStores))
+
+
 
 module.exports = router;
