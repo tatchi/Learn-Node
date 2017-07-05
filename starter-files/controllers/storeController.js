5b969ddfc634dc3c5e1d176a383cf26d37622688
Corentin Leruth <clt@pulsar.be>
2017-07-05 23:47:16 +0200

    Pagination

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -51,8 +51,25 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-  const stores = await Store.find();
-  res.render('stores', { title: 'Stores', stores });
+  
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = (page * limit) - limit
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' });
+  const countPromise = Store.count();
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash('info', 'you asked for a wrong page');
+    res.redirect(`/stores/page/${pages}`)
+    return;
+  }
+  res.render('stores', { title: 'Stores', stores, pages, count, page });
 };
 
 exports.editStore = async (req, res) => {
